b23a0498a99afe46ef6e0aade7ae8bfa0a245a1a
salahdidi <104596961+salahdidi@users.noreply.github.com>
2026-01-17 10:20:36 +0100

    test

--- database/migrations/2026_01_16_103519_add_role_to_user.php
+++ database/migrations/2026_01_16_103519_add_role_to_user.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::table('users', function (Blueprint $table) {
             $table->string('role')->default('user');
         });
+        ///this is a test comment
 
     }
 
